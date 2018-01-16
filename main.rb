05b2ece4d95b0c67b851c9b66c299429b765842e
Sophie McDonald <sophiermcdonald@gmail.com>
2018-01-16 20:45:49 +1100

    add routes for index, show, create form and adventure display

---
+++ main.rb
@@ -0,0 +1,37 @@
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+require_relative 'db_config'
+require_relative 'models/adventure'
+require_relative 'models/tag'
+require_relative 'models/comment'
+require_relative 'models/user'
+
+
+get '/' do
+   @adventure = Adventure.all
+  erb :index
+end
+
+get '/adventures/new' do
+  erb :new
+end
+
+post '/adventures' do
+adventure = Adventure.new
+adventure.name = params[:name]
+adventure.image_url = params[:image_url]
+adventure.address = params[:address]
+adventure.details = params[:details]
+adventure.special_mention = params[:special_mention]
+adventure.save
+redirect '/'
+  # http://www.banyulenillumbikkids.com.au/web/wp-content/uploads/2016/08/park.jpg
+end
+
+
+get '/adventures/:id' do
+  @adventure = Adventure.find(params[:id])
+  erb :show
+end
