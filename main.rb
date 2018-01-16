a5be31a1ff2dad6ef46e47d363deb89a894d0aed
Sophie McDonald <sophiermcdonald@gmail.com>
2018-01-16 20:59:48 +1100

    Add ability to save edited adventure

--- main.rb
+++ main.rb
@@ -13,7 +13,7 @@ get '/' do
    @adventure = Adventure.all
   erb :index
 end
-
+#Create
 get '/adventures/new' do
   erb :new
 end
@@ -30,16 +30,28 @@ redirect '/'
   # http://www.banyulenillumbikkids.com.au/web/wp-content/uploads/2016/08/park.jpg
 end
 
-
+#read
 get '/adventures/:id' do
   #   redirect '/login' unless logged_in?
   @adventure = Adventure.find(params[:id])
   erb :show
 end
 
+#update
 get'/adventures/:id/edit' do
   @adventure = Adventure.find(params[:id])
   erb :edit
+end
+
+put '/adventures/:id' do
+  adventure = Adventure.find(params[:id])
+  adventure.name = params[:name]
+  adventure.image_url = params[:image_url]
+  adventure.address = params[:address]
+  adventure.details = params[:details]
+  adventure.special_mention = params[:special_mention]
+  adventure.save
+  redirect "adventures/#{params[:id]}"
 end
 
 delete '/adventures/:id' do
@@ -47,58 +59,3 @@ delete '/adventures/:id' do
   adventure.destroy
   redirect '/'
 end
-#
-# put '/dishes/:id' do
-#   dish = Dish.find(params[:id])
-#   dish.name = params[:name]
-#   dish.image_url = params[:image_url]
-#   dish.save
-#   #redirect "dishes/' + #{params[:id]}"
-#   redirect "dishes/#{params[:id]}"
-# end
-#
-# post '/comments' do
-#   comment = Comment.new
-#   comment.body = params[:body]
-#   comment.dish_id = params[:dish_id]
-#   comment.save
-#   redirect "/dishes/#{comment.dish_id}"
-# end
-#
-# get '/login' do
-#   erb :login
-# end
-#
-# post '/session' do
-#   #check email exists
-#   user = User.find_by(email: params[:email])
-#   #have a user and authenticate return truthy
-#   #check password
-#   if user && user.authenticate(params[:password])
-#     session[:user_id] = user.id #just a hash
-#     redirect '/'
-#   else
-#     erb :login
-#   end
-# end
-#
-# delete '/session' do
-#   session[:user_id] = nil
-#   redirect '/login'
-# end
-#
-#
-# helpers do
-#   def current_user
-#     User.find_by(id: session[:user_id])
-#   end
-#
-#   def logged_in? #this method returns a boolean
-#     !!current_user # !! negates the current_user converting it to a true or false
-#   #   if session[:user_id]
-#   #     return true
-#   #   else
-#   #     return false
-#   #   end
-#   end
-# end
