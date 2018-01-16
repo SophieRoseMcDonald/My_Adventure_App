fbd28c1865b000ba6458ff1e0a0f4c1353a9ba07
Sophie McDonald <sophiermcdonald@gmail.com>
2018-01-16 20:55:37 +1100

    Add delete feature

--- main.rb
+++ main.rb
@@ -41,3 +41,64 @@ get'/adventures/:id/edit' do
   @adventure = Adventure.find(params[:id])
   erb :edit
 end
+
+delete '/adventures/:id' do
+  adventure = Adventure.find(params[:id])
+  adventure.destroy
+  redirect '/'
+end
+#
+# put '/dishes/:id' do
+#   dish = Dish.find(params[:id])
+#   dish.name = params[:name]
+#   dish.image_url = params[:image_url]
+#   dish.save
+#   #redirect "dishes/' + #{params[:id]}"
+#   redirect "dishes/#{params[:id]}"
+# end
+#
+# post '/comments' do
+#   comment = Comment.new
+#   comment.body = params[:body]
+#   comment.dish_id = params[:dish_id]
+#   comment.save
+#   redirect "/dishes/#{comment.dish_id}"
+# end
+#
+# get '/login' do
+#   erb :login
+# end
+#
+# post '/session' do
+#   #check email exists
+#   user = User.find_by(email: params[:email])
+#   #have a user and authenticate return truthy
+#   #check password
+#   if user && user.authenticate(params[:password])
+#     session[:user_id] = user.id #just a hash
+#     redirect '/'
+#   else
+#     erb :login
+#   end
+# end
+#
+# delete '/session' do
+#   session[:user_id] = nil
+#   redirect '/login'
+# end
+#
+#
+# helpers do
+#   def current_user
+#     User.find_by(id: session[:user_id])
+#   end
+#
+#   def logged_in? #this method returns a boolean
+#     !!current_user # !! negates the current_user converting it to a true or false
+#   #   if session[:user_id]
+#   #     return true
+#   #   else
+#   #     return false
+#   #   end
+#   end
+# end
