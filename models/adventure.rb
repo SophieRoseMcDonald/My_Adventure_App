ad7656cc81fb520a885ff174b44399a69cedb32a
Sophie McDonald <sophiermcdonald@gmail.com>
2018-02-06 15:29:12 +1100

    change url name

--- models/adventure.rb
+++ models/adventure.rb
@@ -2,7 +2,6 @@ class Adventure < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   def self.search(search)
-    where("name like ?", "%#{search}%")
+    where('name like ?', "%#{search}%")
   end
-
 end
